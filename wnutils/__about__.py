a17e992b8777d3d0d89fa930d00d5f77ff47a382
Bradley Meyer <mbradle@clemson.edu>
2018-07-11 08:00:26 -0400

    Update for release 1.3.0.

--- wnutils/__about__.py
+++ wnutils/__about__.py
@@ -7,6 +7,6 @@ __all__ = [
 
 __title__ = 'wnutils'
 __summary__ = 'Python project to read and plot webnucleo files'
-__version__ = '1.3.0dev'
+__version__ = '1.3.0'
 __author__ = 'Clemson University'
 __copyright__ = 'Clemson University, 2018'
