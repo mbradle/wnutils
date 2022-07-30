e15f730360d3ac57c7e82b115d688b33f14b9b0a
Bradley Meyer <mbradle@clemson.edu>
2022-07-30 08:18:17 -0400

    Update version.

--- wnutils/__about__.py
+++ wnutils/__about__.py
@@ -2,6 +2,6 @@ __all__ = ["__title__", "__summary__", "__version__", "__author__", "__copyright
 
 __title__ = "wnutils"
 __summary__ = "Python project to read and plot webnucleo files"
-__version__ = "2.4.0"
+__version__ = "2.4.1"
 __author__ = "Clemson University"
 __copyright__ = "Clemson University, 2018-2022"
